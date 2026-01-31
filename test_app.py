68fce20fc03b3045455a1cccf9bb679e1031cc58
DBT-Stewart <stewartprince.PM@diggibyte.com>
2026-01-31 21:20:23 +0530

    Added multiplication table

--- test_app.py
+++ test_app.py
@@ -1,7 +1,10 @@
-from app import add,sub
+from app import add, mul, sub
 
 def test_add():
     assert add(2, 3) == 5
 
 def test_sub():
     assert sub(3, 2) == 1
+
+def test_mul():
+    assert mul(2, 3) == 6
